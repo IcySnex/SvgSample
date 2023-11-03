5e9cfe9b73c958460bd07f236912cf895c9ba9a1
IcySnex <81762750+IcySnex@users.noreply.github.com>
2023-11-03 11:29:13 +0100

    Added ability to load svg from file (WORKS)

--- SvgSampleLibrary/CustomDictionary.xaml.cs
+++ SvgSampleLibrary/CustomDictionary.xaml.cs
@@ -15,6 +15,10 @@ namespace SvgSampleLibrary
         public static void LoadIcon(
             ResourceDictionary dic)
         {
+            // Loading from file (WORKS):
+            //((Style)dic["DefaultCustomElementStyle"]).Setters.Add(new Setter(CustomElement.IconProperty, Helpers.FromAssets("ms-appx:///Assets/sample.svg", 64, 64)));
+
+            // Loading from memory (DOESNT WORK):
             ((Style)dic["DefaultCustomElementStyle"]).Setters.Add(new Setter(CustomElement.IconProperty, Helpers.ToImageSource(Helpers.SampleSvg, 64, 64)));
         }
     }
--- SvgSampleLibrary/Helpers.cs
+++ SvgSampleLibrary/Helpers.cs
@@ -27,6 +27,19 @@ namespace SvgSampleLibrary
             }
         }
 
+        public static ImageSource FromAssets(
+            string svgPath,
+            int width,
+            int height)
+        {
+            SvgImageSource imageSource = new SvgImageSource(new Uri(svgPath))
+            {
+                RasterizePixelWidth = width,
+                RasterizePixelHeight = height,
+            };
+            return imageSource;
+        }
+
 
         public const string SampleSvg = "<svg xmlns=\"http://www.w3.org/2000/svg\" baseProfile=\"tiny\" viewBox=\"0 0 128 128\" overflow=\"visible\" fill=\"#fff\" xmlns:v=\"https://vecta.io/nano\"><path d=\"M63.9 65.6C81.7 65.6 96 51.2 96 33.4c0-17.6-14.4-32-32.1-32S32 15.9 32 33.6c0 17.6 14.3 32 31.9 32zm0-50.7c10.4 0 18.6 8.4 18.6 18.6 0 10.4-8.4 18.6-18.6 18.6s-18.6-8.4-18.6-18.6 8.4-18.6 18.6-18.6zm0 57.7C28.6 72.6 0 87 0 104.7v21.8h128v-21.8c0-17.7-28.8-32.1-64.1-32.1zm51.5 34.7v4.6c0 2-1.8 3.6-4 3.6H16.6c-2.2 0-4-1.6-4-3.6v-4.6-.6-.8c0-11.6 23-21 51.5-21 28.4 0 51.5 9.4 51.5 21v.8c-.2.4-.2.6-.2.6z\"/></svg>";
     }
